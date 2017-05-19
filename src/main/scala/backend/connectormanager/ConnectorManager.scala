ce7f72da71ea777e7cbc590db9b5f22314315343
Pavel Zaytsev <pzaytsev@Pavel-Zaytsevs-MacBook-Pro.local>
2017-05-19 16:31:43 -0700

    Initials states and data for ConnectorManager

--- src/main/scala/backend/connectormanager/ConnectorManager.scala
+++ src/main/scala/backend/connectormanager/ConnectorManager.scala
@@ -4,16 +4,27 @@ import backend.connector.Connector
 import backend.connector.Connector.Endpoint
 /// Hierarchical structure
 
-/// Should initiate some set of connectors
+/// Should initiate some set of connectors -> monitors connectors like it's children
+
+/// Gives underlying commands to start building and materializing flows
 
 /// Should know the failures and how to handle
 
-/// Shoud use FSM module
+/// Should use FSM module
 
 //// Dependency Injection
 
 sealed trait State
 sealed trait Data
+
+case class StateData(endpoints: List[Endpoint]) extends Data
+case object Idle extends State
+case object CreationPending extends State
+case object Created extends State
+case object ConnectionPending extends State
+case object Connected extends State
+case object Disconnected extends State
+
 
 object ConnectorManager {
   def props_self(): Props = Props(new ConnectorManager)
@@ -53,5 +64,5 @@ class ConnectorManager extends FSM[State, Data] with Stash{
     }
   }
 
-  // def receive = ???
+
 }
