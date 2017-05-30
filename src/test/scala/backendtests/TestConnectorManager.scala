426b1d90f02848bf9230115051d92a784b5d19e3
Pavel Zaytsev <pzaytsev@Pavel-Zaytsevs-MacBook-Pro.local>
2017-05-30 14:24:59 -0700

    Adding tests for TestConnectorManager

---
+++ src/test/scala/backendtests/TestConnectorManager.scala
@@ -0,0 +1,16 @@
+package backendtests
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
+
+/**
+  * Created by pzaytsev on 5/30/17.
+  */
+class TestConnectorManager extends TestKit(ActorSystem("test_system")) with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll{
+
+override  def afterAll: Unit = {
+  TestKit.shutdownActorSystem(system)
+}
+
+}
