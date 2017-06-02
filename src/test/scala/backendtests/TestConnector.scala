1b138bb6cc03eac93c84c63e30d2ff8df0721b57
Pavel Zaytsev <pzaytsev@Pavel-Zaytsevs-MacBook-Pro.local>
2017-06-02 00:48:17 -0700

    Added unit test

--- src/test/scala/backendtests/TestConnector.scala
+++ src/test/scala/backendtests/TestConnector.scala
@@ -1,5 +1,6 @@
 package backendtests
 
+import scala.concurrent.duration._
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
@@ -22,6 +23,16 @@ class TestConnector extends TestKit(ActorSystem("test_system")) with ImplicitSen
       connector.tell(StreamRequestStart("123", "456"), probe.ref)
       val msg = probe.expectMsg(ConnectorRegistered)
       probe.lastSender should === (connector)
+
+    }
+
+    "Ignoring wrong registration messages when either cmId or connector id are wrong" in {
+      val probe = TestProbe()
+      val connector = system.actorOf(props_connector("123", "456", None))
+      connector.tell(StreamRequestStart("wrongcmid", "456"), probe.ref)
+      probe.expectNoMsg(500.milliseconds)
+      connector.tell(StreamRequestStart("123", "wrongid"), probe.ref)
+      probe.expectNoMsg(500.milliseconds)
 
     }
   }
