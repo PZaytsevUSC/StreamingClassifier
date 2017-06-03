1f2885a1f3fab014af31ce4e5b86774158c44eb0
Pavel Zaytsev <pzaytsev@Pavel-Zaytsevs-MacBook-Pro.local>
2017-06-03 14:08:21 -0700

    Refactor some streaming methods that might be useful

--- src/main/scala/backend/connector/ConnectorBaseStreamEndpointStage.scala
+++ src/main/scala/backend/connector/ConnectorBaseStreamEndpointStage.scala
@@ -16,69 +16,66 @@ import scala.util.Random
 /**
   * Created by pzaytsev on 6/2/17.
   */
-abstract class ConnectorBaseStreamEndpointStage[T] extends GraphStage[SourceShape[T]]{
 
-  val out: Outlet[T] = Outlet("Abstract Source")
-  override val shape: SourceShape[T] = SourceShape(out)
-  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic
+object Helpers {
 
-}
-
-
-object Playground extends App {
-  implicit val system = ActorSystem("QuickStart")
-  implicit val ec = system.dispatcher
-  implicit val materializer = ActorMaterializer()
+  def redirect(a: String):Int = {
+    a.split(",")(1).toInt
+  }
 
   def datasource(f: String): Source[ByteString, Future[IOResult]] = {
     FileIO.fromPath(Paths.get(f))
+  }
+
+  def datasource_framed(f: String): Source[String, Future[IOResult]] = {
+    datasource(f).via(Framing.delimiter(ByteString("\n"), 256, true)).map(_.utf8String)
   }
 
   def datasink(f: String): Sink[String, Future[IOResult]] = {
     Flow[String].map(s => ByteString(s + "\n")).toMat(FileIO.toPath(Paths.get(f)))(Keep.right)
   }
 
-  def get_random_class(): String = {
-    Math.abs(Random.nextInt()) % 5 toString()
-  }
+  def split_to_sources_by_category_graph_component(num_of_categories: Int, source: Source[String, Future[IOResult]]): RunnableGraph[NotUsed] = {
 
-  // Source((1 to 100) map (_ => get_random_class()) ).runForeach(println)
+    var sinks: Map[Int, Sink[String, Future[IOResult]]] = Map.empty[Int, Sink[String, Future[IOResult]]]
 
-
-  val ds = datasource("file2.txt").via(Framing.delimiter(ByteString("\n"), 256, true)).map(_.utf8String)
-//
-//  ds.zipWith(Source((0 to 100) map (_ => get_random_class()) ))((x, y) => s"$x,$y")
-//    .map(x => ByteString(x + "\n"))
-//    .runWith(FileIO.toPath(Paths.get("file2.txt")))
-//    .onComplete(_ => println("complete"))
-
-
-  // write in different sinks depending on number of classes
-  def redirect(a: String):Int = {
-    a.split(",")(1).toInt
-  }
-
-  var sinks: Map[Int, Sink[String, Future[IOResult]]] = Map.empty[Int, Sink[String, Future[IOResult]]]
-
-  (0 to 4)foreach{
-    x => sinks += x -> datasink(x + ".txt")
-  }
-
-  // Good code for category stream fan out
-  val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
-    val bcast = b.add(Broadcast[String](4))
-    ds ~> bcast.in
-    (0 to 3) foreach {
-       x => bcast.out(x) ~> Flow[String].filter(y => x == redirect(y)) ~> sinks.get(x).getOrElse(datasink("nonclassified.txt"))
-
+    (0 to num_of_categories)foreach{
+      x => sinks += x -> datasink(x + ".txt")
     }
-    ClosedShape
-  })
+
+    val graph: RunnableGraph[NotUsed] = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+      val bcast = b.add(Broadcast[String](num_of_categories + 1))
+      source ~> bcast.in
+      (0 to num_of_categories) foreach {
+        x => bcast.out(x) ~> Flow[String].filter(y => x == redirect(y)) ~> sinks.get(x).getOrElse(datasink("nonclassified.txt"))
+
+      }
+      ClosedShape
+    })
+
+    graph
+  }
+
+}
+
+abstract class ConnectorBaseStreamEndpointStage[T] extends GraphStage[SourceShape[T]]{
+
+  val out: Outlet[T] = Outlet("Abstract Source")
+  override val shape: SourceShape[T] = SourceShape(out)
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic
 
 
-  g.run()
 
+}
+
+
+
+
+object Playground extends App {
+  implicit val system = ActorSystem("QuickStart")
+  implicit val ec = system.dispatcher
+  implicit val materializer = ActorMaterializer()
 
 
 
