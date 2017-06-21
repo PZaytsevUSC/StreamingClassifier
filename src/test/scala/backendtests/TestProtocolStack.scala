ca69945f2f501e46aa0b17325fbfe117b26ad86f
Pavel Zaytsev <pzaytsev@Pavel-Zaytsevs-MacBook-Pro.local>
2017-06-21 14:57:32 -0700

    Redefine codec protocol, fix tests

--- src/test/scala/backendtests/TestProtocolStack.scala
+++ src/test/scala/backendtests/TestProtocolStack.scala
@@ -5,9 +5,7 @@ import java.nio.ByteOrder
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Flow
-import akka.stream.scaladsl.Sink
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.stream.testkit.TestSubscriber.Probe
 import akka.stream.testkit.scaladsl.TestSink
 import akka.testkit.TestKit
@@ -32,13 +30,13 @@ class TestProtocolStack extends TestKit(ActorSystem("test_system", ConfigFactory
   "A codec stage" must {
 
     "parse ping and pong messages" in {
-      val pings: Source[ByteString, NotUsed] = Source(1 to 10).map(ByteString.newBuilder.putByte(1).putInt(_).result())
+      val pings: Source[ByteString, NotUsed] = Source(1 to 10).map(x => ByteString("p:" + x))
       val flow: Flow[ConnectorDialect, ConnectorDialect, NotUsed] = Flow[ConnectorDialect].collect {case Ping(id) => Pong(id)}
       val codecflow = CodecStage() join (flow)
       val probe: Probe[ByteString] = pings.via(codecflow).runWith(TestSink.probe[ByteString])
       val result = probe.request(10).expectNextN(10)
       assert(result.length == 10)
-      assert(result.map(b => b.iterator.getByte).filter(x => x != 2).isEmpty)
+      assert(result.map(x => x.utf8String.trim.charAt(0)).filter(x => x != 'o').isEmpty)
     }
   }
 
